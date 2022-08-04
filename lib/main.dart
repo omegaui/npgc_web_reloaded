cc843f980c4f19c9b3ba31de706cefc080f6f0da
omegaui <omegaui22@gmail.com>
2022-08-04 16:18:22 +0530

    Finished Home Page!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:npgc_web_reloaded/pages/home/home_page.dart';
 import 'package:npgc_web_reloaded/widgets/hover_panel.dart';
@@ -36,6 +37,14 @@ class ContentPane extends StatelessWidget {
       child: Stack(
         children: [
           Align(
+            alignment: Alignment.centerLeft,
+            child: Image.asset('assets/tech.png'),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Image.asset('assets/clip-411.png'),
+          ),
+          Align(
             alignment: Alignment.topRight,
             child: Image.network('https://img.icons8.com/color/240/000000/world-map-continents.png'),
           ),
@@ -50,9 +59,169 @@ class ContentPane extends StatelessWidget {
             primary: true,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                TopPanel(),
-                SingleChildScrollView(
+              children: [
+                const TopPanel(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 1200,
+                      height: 500,
+                      child: ImageSlideshow(
+                        width: 1140,
+                        height: 450,
+                        initialPage: 0,
+                        indicatorColor: Colors.blue,
+                        indicatorBackgroundColor: Colors.grey,
+                        autoPlayInterval: 10000,
+                        isLoop: true,
+                        children: [
+                          Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: const FadeInImage(
+                                  placeholder: AssetImage('assets/pluto-250.png'),
+                                  image: NetworkImage('https://npgc.in/assets/images/slides/slide-3.jpg'),
+                                ),
+                              ),
+                              Text(
+                                "Grade 'A' Accredited by NAAC & Autonomous and College with Potential for Excellence(CPE) by UGC",
+                                style: TextStyle(
+                                  color: Colors.grey.shade800,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: const FadeInImage(
+                                  placeholder: AssetImage('assets/pluto-250.png'),
+                                  image: NetworkImage('https://npgc.in/assets/images/slides/slide-2.jpg'),
+                                ),
+                              ),
+                              Text(
+                                "We Design the Future and Set Trends for others to follow.",
+                                style: TextStyle(
+                                  color: Colors.grey.shade800,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Center(
+                  child: Wrap(
+                    spacing: 40,
+                    alignment: WrapAlignment.center,
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(primary: Colors.red),
+                        onPressed: () {},
+                        child: Column(
+                          children: [
+                            Image.network('https://img.icons8.com/cute-clipart/128/000000/hydroelectric.png'),
+                            Text(
+                              "NAAC 'A'",
+                              style: TextStyle(
+                                color: Colors.grey.shade800,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(primary: Colors.deepPurple),
+                        onPressed: () {},
+                        child: Column(
+                          children: [
+                            Image.network('https://img.icons8.com/cute-clipart/128/000000/diploma.png'),
+                            Text(
+                              "Courses",
+                              style: TextStyle(
+                                color: Colors.grey.shade800,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(primary: Colors.blue),
+                        onPressed: () {},
+                        child: Column(
+                          children: [
+                            Image.network('https://img.icons8.com/cute-clipart/128/000000/tear-off-calendar.png'),
+                            Text(
+                              "Examinations",
+                              style: TextStyle(
+                                color: Colors.grey.shade800,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(primary: Colors.blueGrey),
+                        onPressed: () {},
+                        child: Column(
+                          children: [
+                            Image.network('https://img.icons8.com/cute-clipart/128/000000/terms-and-conditions.png'),
+                            Text(
+                              "Forms",
+                              style: TextStyle(
+                                color: Colors.grey.shade800,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(primary: Colors.yellowAccent),
+                        onPressed: () {},
+                        child: Column(
+                          children: [
+                            Image.network('https://img.icons8.com/cute-clipart/128/000000/test-passed.png'),
+                            Text(
+                              "Results",
+                              style: TextStyle(
+                                color: Colors.grey.shade800,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(primary: Colors.deepOrange),
+                        onPressed: () {},
+                        child: Column(
+                          children: [
+                            Image.network('https://img.icons8.com/cute-clipart/128/000000/tripadvisor.png'),
+                            Text(
+                              "Alumni",
+                              style: TextStyle(
+                                color: Colors.grey.shade800,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: HomePage(),
                 ),
